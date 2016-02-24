6d653ec2e06d6be77cd2af7f89c4de9fe8d79479
aardouin <alexis.ardouin@gmail.com>
2016-02-24 17:40:54 +0100

    fix %@ to %sé

--- core/Transformer.js
+++ core/Transformer.js
@@ -43,6 +43,7 @@ var androidTransformer = {
         var normalizedValue = value.replace(/%newline%/gi, "\\n");
         normalizedValue = normalizedValue.replace(/'/gi, "\\'");
         normalizedValue = normalizedValue.replace(/%([sdf])/gi, '%#$$$1');
+        normalizedValue = normalizedValue.replace(/%@/gi, '%s');
         normalizedValue = normalizedValue.replace(/&/gi, "&amp;");
         normalizedValue = normalizedValue.replace(/\u00A0/gi, "\\u00A0");
         normalizedValue = normalizedValue.replace(/([^\.]|^)(\.{3})([^\.]|$)/gi, '$1&#8230;$3');
